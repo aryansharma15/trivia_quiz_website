ac4748ff77f67a2744dfe4ea2becab6329c085ee
Aryan Sharma <65057388+aryansharma15@users.noreply.github.com>
2023-08-17 13:38:54 +0530

    UI & working on categories

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-const api = "https://the-trivia-api.com/v2/questions";
+document.addEventListener("DOMContentLoaded", () => {
+	const api = "https://the-trivia-api.com/v2/questions";
+	const selDiffic = document.getElementById("diffic");
+	const selCateg = document.getElementById("categ");
+
+	console.log(api);
+	console.log(selDiffic);
+	console.log(selCateg);
+
+	function selBackground(selection) {
+		if (selection.value !== "") {
+			selection.style.backgroundColor = "#b8fbcd";
+		}
+
+		console.log("selBack was run");
+	}
+
+	selDiffic.addEventListener("change", () => {
+		selBackground(selDiffic);
+	});
+
+	// function listeners() {
+	// 	selDiffic.addEventListener("change", selBackground);
+	// }
+});
