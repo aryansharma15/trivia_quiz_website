68446094487201153c9e1d9c1d9ddc79dcb216cd
Aryan Sharma <65057388+aryansharma15@users.noreply.github.com>
2023-08-17 22:41:13 +0530

    category update, adding functionality to display ques

--- script.js
+++ script.js
@@ -2,22 +2,38 @@ document.addEventListener("DOMContentLoaded", () => {
 	const api = "https://the-trivia-api.com/v2/questions";
 	const selDiffic = document.getElementById("diffic");
 	const selCateg = document.getElementById("categ");
+	const startBtn = document.getElementsByClassName("start-btn");
 
 	console.log(api);
 	console.log(selDiffic);
 	console.log(selCateg);
+	console.log(startBtn);
 
 	function selBackground(selection) {
-		if (selection.value !== "") {
-			selection.style.backgroundColor = "#b8fbcd";
+		if (selection.value === "") {
+			selection.classList.remove("selected");
+		} else {
+			selection.classList.add("selected");
 		}
-
-		console.log("selBack was run");
 	}
 
-	selDiffic.addEventListener("change", () => {
-		selBackground(selDiffic);
-	});
+	function listeners() {
+		selDiffic.addEventListener("input", () => {
+			selBackground(selDiffic);
+			selDiffic.blur();
+		});
+
+		selCateg.addEventListener("input", () => {
+			selBackground(selCateg);
+			selCateg.blur();
+		});
+
+		startBtn.addEventListener("click", () => {});
+	}
+
+	window.onload = () => {
+		listeners();
+	};
 
 	// function listeners() {
 	// 	selDiffic.addEventListener("change", selBackground);
