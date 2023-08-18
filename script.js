662d03536c007a4cb73f0c8c4766e3b7bef39322
Aryan Sharma <65057388+aryansharma15@users.noreply.github.com>
2023-08-18 13:58:22 +0530

    checked alerts

--- script.js
+++ script.js
@@ -2,12 +2,13 @@ document.addEventListener("DOMContentLoaded", () => {
 	const api = "https://the-trivia-api.com/v2/questions";
 	const selDiffic = document.getElementById("diffic");
 	const selCateg = document.getElementById("categ");
-	const startBtn = document.getElementsByClassName("start-btn");
+	const startBtn = document.getElementsByClassName("start-btn")[0];
+	const landingContent = document.getElementsByClassName("top-content")[0];
+	const quizSect = document.getElementsByClassName("quiz-section")[0];
 
-	console.log(api);
-	console.log(selDiffic);
-	console.log(selCateg);
-	console.log(startBtn);
+	const alert = document.getElementById("custom-modal");
+	const alertMessage = document.getElementById("modal-message");
+	const alertBtn = document.getElementById("modal-close-btn");
 
 	function selBackground(selection) {
 		if (selection.value === "") {
@@ -18,6 +19,9 @@ document.addEventListener("DOMContentLoaded", () => {
 	}
 
 	function listeners() {
+		alertBtn.addEventListener("click", () => {
+			alert.style.display = "none";
+		});
 		selDiffic.addEventListener("input", () => {
 			selBackground(selDiffic);
 			selDiffic.blur();
@@ -28,14 +32,30 @@ document.addEventListener("DOMContentLoaded", () => {
 			selCateg.blur();
 		});
 
-		startBtn.addEventListener("click", () => {});
+		startBtn.addEventListener("click", () => {
+			const selectedCategory = selCateg.value;
+			const selectedDifficulty = selDiffic.value;
+			if (selectedCategory !== "" && selectedDifficulty !== "") {
+				landingContent.style.display = "none";
+				quizSect.style.display = "block";
+				console.log(selectedCategory);
+				console.log(selectedDifficulty);
+			} else if (selectedCategory === "" && selectedDifficulty !== "") {
+				showAlert("Please select a valid category to continue.");
+			} else if (selectedCategory !== "" && selectedDifficulty === "") {
+				showAlert("Please select a valid difficulty to continue.");
+			} else {
+				showAlert("Please choose a category and a difficulty before continuing.");
+			}
+		});
+	}
+
+	function showAlert(message) {
+		alert.style.display = "flex";
+		alertMessage.textContent = message;
 	}
 
 	window.onload = () => {
 		listeners();
 	};
-
-	// function listeners() {
-	// 	selDiffic.addEventListener("change", selBackground);
-	// }
 });
